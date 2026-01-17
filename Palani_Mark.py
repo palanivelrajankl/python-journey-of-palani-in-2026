083bc4ed4b92f7610c4a383f2b5e0c82f116a5be
Palanivelrajan <palanivelrajan.k.l@gmail.com>
2026-01-17 10:18:13 +0530

    Adding Logic for Palani's Mark Journey! 📉
    Experimenting with how variables change their values. Even if the Mark becomes 0, the logic Stays Strong! HEyy! ;)

---
+++ Palani_Mark.py
@@ -0,0 +1,6 @@
+# HEyy...Now u Can see That here These are Variables 
+Palani_Mark = 10
+print("Palani Mark","=",Palani_Mark)
+#NOw Let's Try Some Little Math with Variables;)...
+Palani_Mark = 10-10
+print("Palani Mark","=",Palani_Mark)
