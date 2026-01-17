94795455647fa287cd420b4b368941b21fa26b68
Palanivelrajan <palanivelrajan.k.l@gmail.com>
2026-01-17 13:43:14 +0530

    HEyy! I built a Destiny Simulator: The Mam/Sir Game 🎓✨
    HEyy! The Teacher's Choice is Live! 🎓✨
    This isn't just a script; it’s a "Destiny Simulator." Today, I learned that coding is about Power. I give the power to the user to be my "Mam or Sir" and decide my fate.
    The Battle with the int() Ghost:
    I almost got defeated by a ValueError today. I tried to force the computer to turn "6/6 Pass" into a number, but Python said "No way!" I realized that my Teacher's words are Strings, not just math. I fixed it, and now the conversation flows perfectly.
    The Heart of the Code:
    If you make me pass, I'm happy. If you fail me... well, the code tells you exactly what I think of you! ;) It’s interactive, it’s alive, and it’s uniquely Palani.

---
+++ Exam_Teacher_Game.py
@@ -0,0 +1,47 @@
+#HEyy...Now tym for the Exam Result With a little detail About ME...
+Name = "Palani"
+Hight = "164cm"
+print("It's THe Most Intersting Part You are gonna be MY Mam/Sir Unstill the code gonna End/n"
+"And you can choose MY MArk")
+print("Here as a Mam/Sir YOU Need to Follow SOme Rules(It's Nessasory)")
+print("*THe Exam only Enter in the out 6 Way...(For Example:- 6/6 pass)")
+print("* YOu can control That How Much Pass I CAn")
+#1st CA Result
+print("1st CA")
+_1st_CA_Exam_Result =(input())
+print("1st CA Exam Result = ",_1st_CA_Exam_Result)
+if _1st_CA_Exam_Result == "6/6 Pass":
+    print("Palani Gud boy")
+else:
+    print("Palani Bad boy")
+#2st CA Result
+print("2nd CA")
+_2nd_CA_Exam_Result =(input())
+print("2nd CA Exam Result = ",_2nd_CA_Exam_Result)
+if _2nd_CA_Exam_Result == "6/6 Pass":
+    print("Palani Gud boy")
+else:
+    print("Palani Bad boy")
+#SEM Result
+print("SEM Resut")
+print("Please make Me PAss in all Subjects THis one tym i can't took ARREAR")
+SEM_Exam_Result =(input())
+print("SEM Exam Result=",SEM_Exam_Result)
+if SEM_Exam_Result == "6/6 Pass":
+    print("Palani Gud boy")
+else:
+    print("Palani Bad boy")
+if SEM_Exam_Result == "6/6 Pass":
+    print("THank you MAm/Sir YOU are the BEst..")
+else:
+    print("YOu are the Worst Mam/Sir I EVer Seen...;(")
+print("OVer All DEtails")
+# Over all look Just For an REvise...
+print(f"My Name is {Name}\n",
+f"I am {Hight},Tall!...\n"
+f"First CA Result-{_1st_CA_Exam_Result}\n",
+f"Second CA Result-{_2nd_CA_Exam_Result}\n",
+f"SEM Result-{SEM_Exam_Result}")
+print("Here the end GUd byee.......................................")
+
+
